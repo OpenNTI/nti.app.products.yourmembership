d157bc884f0da6b8687a4ab8a70e839351879d6f
cutz <christopher.m.utz@gmail.com>
2018-11-07 16:24:22 -0600

    Initial project files

---
+++ setup.py
@@ -0,0 +1,78 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.app.testing',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.products.yourmembership',
+    version=_read('version.txt').strip(),
+    author='Chris Utz',
+    author_email='chris@nextthought.com',
+    description="NTI YourMembershipr",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='Products YourMembership',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.products.yourmembership",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app', 'nti.app.products'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'pyramid',
+        'six',
+        'nti.app.products.integration',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.schema',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
